f61a6e1d71513400f0a68d5297e017631a259381
Meir Miyara <3089864+mase1981@users.noreply.github.com>
2025-09-15 15:57:43 -0500

    fix

--- uc_intg_skyq/__init__.py
+++ uc_intg_skyq/__init__.py
@@ -2,14 +2,14 @@
 SkyQ Integration for Unfolded Circle Remote Two/3.
 
 :copyright: (c) 2025 by Meir Miyara.
-:license: Mozilla Public License Version 2.0, see LICENSE for more details.
+:license: MIT, see LICENSE for more details.
 """
 
 try:
     from ._version import version as __version__
     from ._version import version_tuple
 except ImportError:
-    __version__ = "1.0.6"
-    version_tuple = (1, 0, 5)
+    __version__ = "unknown version"
+    version_tuple = (0, 0, "unknown version")
 
 __all__ = ["__version__", "version_tuple"]
