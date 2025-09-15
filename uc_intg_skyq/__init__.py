f7b4beb9bcf1c1d6b792cadcfc4caebd6732a2e7
Meir Miyara <3089864+mase1981@users.noreply.github.com>
2025-09-15 15:23:15 -0500

    fix init relevant path and bump ver

--- uc_intg_skyq/__init__.py
+++ uc_intg_skyq/__init__.py
@@ -6,19 +6,10 @@ Author: Meir Miyara
 Email: meir.miyara@gmail.com
 """
 
-import logging  # isort:skip
-import json
-import os
+import logging
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-try:
-    _driver_path = os.path.join(os.path.dirname(__file__), "..", "driver.json")
-    with open(_driver_path, 'r') as f:
-        _driver_data = json.load(f)
-        __version__ = _driver_data["version"]
-        version_tuple = tuple(int(x) for x in __version__.split("."))
-except:
-    # Fallback if driver.json not found
-    __version__ = "1.0.0"
-    version_tuple = (1, 0, 0)
+# Simple version definition - no complex file loading
+__version__ = "1.0.4"
+version_tuple = (1, 0, 0)
